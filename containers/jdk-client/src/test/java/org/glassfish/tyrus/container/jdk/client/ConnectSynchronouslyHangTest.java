e8ea50e857aed9bf64bdbfdf72f22a51a8a8a1b7
Vladimir Golyakov <wolodia@narod.ru>
2021-03-05 14:27:17 +0300

    [646] Added waiting for thread pool shutdown to ConnectSynchronouslyHangTest Bug: https://github.com/eclipse-ee4j/tyrus/issues/646 Signed-off-by: Vladimir Golyakov <wolodia@narod.ru>

--- containers/jdk-client/src/test/java/org/glassfish/tyrus/container/jdk/client/ConnectSynchronouslyHangTest.java
+++ containers/jdk-client/src/test/java/org/glassfish/tyrus/container/jdk/client/ConnectSynchronouslyHangTest.java
@@ -19,15 +19,19 @@ package org.glassfish.tyrus.container.jdk.client;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import javax.websocket.ClientEndpoint;
 
 import org.glassfish.tyrus.client.ClientManager;
+import org.glassfish.tyrus.client.ClientProperties;
 import org.glassfish.tyrus.test.tools.TestContainer;
 
 import org.junit.Test;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Vladimir Golyakov
@@ -36,6 +40,7 @@ public class ConnectSynchronouslyHangTest extends TestContainer {
 
     @Test
     public void testConnectSynchronouslyHang() throws IOException, InterruptedException {
+        final ClientManager client = ClientManager.createClient(JdkClientContainer.class.getName());
         Thread clientThread = null;
         ServerSocket server = startBadServer();
         try {
@@ -43,7 +48,8 @@ public class ConnectSynchronouslyHangTest extends TestContainer {
                 @Override
                 public void run() {
                     try {
-                        ClientManager client = ClientManager.createClient(JdkClientContainer.class.getName());
+                        client.getProperties().put(ClientProperties.HANDSHAKE_TIMEOUT, 1);
+                        client.getProperties().put(ClientProperties.SHARED_CONTAINER_IDLE_TIMEOUT, 1);
                         client.connectToServer(AnnotatedClientEndpoint.class, getURI("/any-endpoint", "wss"));
                     } catch (Exception e) {
                         // ignore
@@ -60,6 +66,7 @@ public class ConnectSynchronouslyHangTest extends TestContainer {
                 clientThread.interrupt();
             }
             server.close();
+            waitForThreadPoolShutdown(client);
         }
     }
 
@@ -87,5 +94,19 @@ public class ConnectSynchronouslyHangTest extends TestContainer {
 
     @ClientEndpoint
     public static class AnnotatedClientEndpoint {
+    }
+
+    private void waitForThreadPoolShutdown(ClientManager client) {
+        try {
+            /* Tests in the package are sensitive to freeing resources. Unclosed sessions might hinder the next test
+            (if the next test requires a fresh client thread pool) */
+            ExecutorService executorService = client.getExecutorService();
+            ScheduledExecutorService scheduledExecutorService = client.getScheduledExecutorService();
+            client.shutdown();
+            assertTrue(executorService.awaitTermination(5, TimeUnit.SECONDS));
+            assertTrue(scheduledExecutorService.awaitTermination(5, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
