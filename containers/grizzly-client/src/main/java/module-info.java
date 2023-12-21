7203291e25b39bc86277e32531603774eb675535
jansupol <jan.supol@oracle.com>
2023-12-21 10:53:13 +0100

    Adopt Jakarta EE 11 dependencies and new Grizzly
    Signed-off-by: jansupol <jan.supol@oracle.com>

--- containers/grizzly-client/src/main/java/module-info.java
+++ containers/grizzly-client/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -21,8 +21,8 @@
 module org.glassfish.tyrus.container.grizzly.client {
     requires java.logging;
     requires jakarta.websocket;
-    requires grizzly.framework;
-    requires grizzly.http;
+    requires org.glassfish.grizzly;
+    requires org.glassfish.grizzly.http;
     requires org.glassfish.tyrus.spi;
     requires org.glassfish.tyrus.client;
     requires org.glassfish.tyrus.core;
