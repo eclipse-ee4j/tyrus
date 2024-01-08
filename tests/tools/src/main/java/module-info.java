7e566d697ab767a0399863a1435885cad27dd9b7
jansupol <jan.supol@oracle.com>
2024-01-08 19:46:56 +0100

    Adopt new Grizzly 4.0.2
    Signed-off-by: jansupol <jan.supol@oracle.com>

--- tests/tools/src/main/java/module-info.java
+++ tests/tools/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2024 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -21,9 +21,9 @@
 module org.glassfish.tyrus.test.tools {
     requires jakarta.websocket;
 
-    requires grizzly.framework;
-    requires grizzly.http;
-    requires grizzly.http.server;
+    requires org.glassfish.grizzly;
+    requires org.glassfish.grizzly.http;
+    requires org.glassfish.grizzly.http.server;
 
     requires junit;
 
