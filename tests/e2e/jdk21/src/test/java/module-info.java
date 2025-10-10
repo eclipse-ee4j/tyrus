28ea80985d51e6a4777771b2a8bf09084d875077
jansupol <jan.supol@oracle.com>
2025-10-10 19:35:58 +0200

    After merge changes
    Signed-off-by: jansupol <jan.supol@oracle.com>

--- tests/e2e/jdk21/src/test/java/module-info.java
+++ tests/e2e/jdk21/src/test/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2025 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -24,6 +24,7 @@ module org.glassfish.tyrus.test.e2e.jdk21 {
     requires junit;
 
     requires org.glassfish.tyrus.client;
+    requires org.glassfish.tyrus.core;
     requires org.glassfish.tyrus.container.grizzly.server;
     requires org.glassfish.tyrus.server;
 
